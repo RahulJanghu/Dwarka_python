8a2c261f1ea4fbecd3f5f774876c6473aa8cb39b
Rahul Janghu <rjanghu01@gmail.com>
2020-03-26 21:33:34 +0530

    10th

---
+++ lec_9/chacha.py
@@ -0,0 +1,10 @@
+def add(a,b):
+    """This is used for summation
+        a: input int value
+        b: input int value
+    """
+    return a + b
+
+def prod(c,d):
+    """This is used for product"""
+    return c * d
